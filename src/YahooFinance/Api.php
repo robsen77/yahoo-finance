f5d2e9870276682cb6425d06f56df1bc3f3ce2d8
Robert Bernhard <bloddynewbie@gmail.com>
2014-05-18 22:34:52 +0200

    refactoring: moved getQuote and getQuotes to an api quote trait

--- src/YahooFinance/Api.php
+++ src/YahooFinance/Api.php
@@ -16,16 +16,18 @@ namespace Robsen77\YahooFinance;
 
 
 use Robsen77\YahooFinance\Config\Config;
-use Robsen77\YahooFinance\Entity\Quote;
 use Robsen77\YahooFinance\Factory\HttpClientFactory as HttpClientFactory;
 use Robsen77\YahooFinance\Factory\ServiceFactory;
 use Robsen77\YahooFinance\Factory\ServiceFactoryMethod;
 use Robsen77\YahooFinance\Http\ClientInterface;
-use Robsen77\YahooFinance\Repository\QuoteCollection;
-use Robsen77\YahooFinance\Service\Quote as QuoteService;
 
 class Api
 {
+    /**
+     * quote api
+     */
+    use \Robsen77\YahooFinance\Api\Quote;
+
     /**
      * @var ClientInterface
      */
@@ -43,39 +45,6 @@ class Api
     {
         $this->initHttpClient($config);
         $this->initServiceFactory();
-    }
-
-    /**
-     * gets a single quote
-     *
-     * wrapper for getQuotes()
-     *
-     * @param $symbol
-     * @return Quote
-     */
-    public function getQuote($symbol)
-    {
-        $quotesCollection = $this->getQuotes(is_array($symbol) ? $symbol : [$symbol]);
-
-        return $quotesCollection[0];
-    }
-
-    /**
-     * gets quote for single symbol or an array of symbols
-     * @param array $symbols
-     * @return QuoteCollection|Quote[]
-     */
-    public function getQuotes($symbols)
-    {
-        if (!is_array($symbols)) {
-            $symbols = [$symbols];
-        }
-
-        /**
-         * @var QuoteService
-         */
-        $service = $this->serviceFactory->create(ServiceFactory::QUOTE);
-        return $service->query($symbols);
     }
 
     /**
---
+++ src/YahooFinance/Api/Quote.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Class Quote
+ * @package Robsen77\YahooFinance\Closure
+ *
+ * @author Robert Bernhard <bloddynewbie@gmail.com>
+ */
+/**
+ * Quote.php
+ *
+ * @author Robert Bernhard <bloddynewbie@gmail.com>
+ */
+
+namespace Robsen77\YahooFinance\Api;
+
+use Robsen77\YahooFinance\Entity\Quote as QuoteEntity;
+use Robsen77\YahooFinance\Factory\ServiceFactory;
+use Robsen77\YahooFinance\Factory\ServiceFactoryMethod;
+use Robsen77\YahooFinance\Repository\QuoteCollection;
+use Robsen77\YahooFinance\Service\Quote as QuoteService;
+
+trait Quote
+{
+    /**
+     * @var ServiceFactoryMethod
+     */
+    private $serviceFactory;
+
+    /**
+     * gets a single quote
+     *
+     * wrapper for getQuotes()
+     *
+     * @param $symbol
+     * @return QuoteEntity
+     */
+    public function getQuote($symbol)
+    {
+        $quotesCollection = $this->getQuotes(is_array($symbol) ? $symbol : [$symbol]);
+
+        return $quotesCollection[0];
+    }
+
+    /**
+     * gets quote for single symbol or an array of symbols
+     * @param array $symbols
+     * @return QuoteCollection|QuoteEntity[]
+     */
+    public function getQuotes($symbols)
+    {
+        if (!is_array($symbols)) {
+            $symbols = [$symbols];
+        }
+
+        /**
+         * @var QuoteService
+         */
+        $service = $this->serviceFactory->create(ServiceFactory::QUOTE);
+        return $service->query($symbols);
+    }
+}
